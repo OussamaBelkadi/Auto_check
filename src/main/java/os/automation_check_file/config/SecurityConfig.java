a704b468ab4096da70af5228331520cbd4b1c62b
Oussama1Belkadi <oussama.belkadi@symolia.com>
2025-05-26 16:16:48 +0100

    enhancing the security layer

--- src/main/java/os/automation_check_file/config/SecurityConfig.java
+++ src/main/java/os/automation_check_file/config/SecurityConfig.java
@@ -5,10 +5,13 @@ import jakarta.servlet.http.HttpSession;
 import lombok.Builder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -19,15 +22,27 @@ import java.util.List;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private JwtAuthConverter jwtAuthConverter;
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity session) throws Exception {
+//        return session
+//                .cors(Customizer.withDefaults())
+//                .csrf(csrf-> csrf.disable())
+//                .sessionManagement(sm->sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                .authorizeHttpRequests(ar->ar.requestMatchers("").permitAll())
+//                .authorizeHttpRequests(ar->ar.anyRequest().authenticated())
+//                .build();
+//    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity session) throws Exception {
         return session
                 .cors(Customizer.withDefaults())
                 .csrf(csrf-> csrf.disable())
                 .sessionManagement(sm->sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//                .authorizeHttpRequests(ar->ar.requestMatchers("").permitAll())
+                .authorizeHttpRequests(ar->ar.requestMatchers("/fileCompare/**").permitAll())
                 .authorizeHttpRequests(ar->ar.anyRequest().authenticated())
-                .oauth2ResourceServer(o2->o2.jwt(Customizer.withDefaults()))
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
     }
 
@@ -35,7 +50,7 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setExposedHeaders(List.of("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
